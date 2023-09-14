0539b4a7f60cfc04976deb1cde34a97c5ff913d8
abdulhamid4663 <abdulhamid4663@gmail.com>
2023-09-15 01:35:51 +0600

    finished the project and edited and readme.md file

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -2,8 +2,6 @@ import PropTypes from 'prop-types';
 import Course from '../Course/Course';
 
 const Courses = ({ courses, handleSelectedCourses }) => {
-    // const {id, cover_img, title, description, price, credit} = courses;
-
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 lg:w-3/4 pb-24'>
             {
